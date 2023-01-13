9392b660637d42447ae2d5a771457d070631ca0f
Douglas White <doug@douglaswhite.app>
2023-01-13 16:15:19 -0600

    add andoid ios update ng
    Signed-off-by: Douglas White <doug@douglaswhite.app>

---
+++ android/app/src/main/assets/public/cordova_plugins.js
@@ -0,0 +1,21 @@
+
+  cordova.define('cordova/plugin_list', function(require, exports, module) {
+    module.exports = [
+      {
+          "id": "cordova-plugin-google-analytics.UniversalAnalytics",
+          "file": "plugins/cordova-plugin-google-analytics/www/analytics.js",
+          "pluginId": "cordova-plugin-google-analytics",
+        "clobbers": [
+          "analytics",
+          "ga"
+        ]
+        }
+    ];
+    module.exports.metadata =
+    // TOP OF METADATA
+    {
+      "cordova-plugin-google-analytics": "1.9.2"
+    };
+    // BOTTOM OF METADATA
+    });
+    
